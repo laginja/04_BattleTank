fede15948ec77b93535c3a3a2db94e1f89b3b229
Smoki-PC\Smoki <matko.smoljan@gmail.com>
2018-06-14 14:10:41 +0200

    BT20 Using Clamp() to limit values - barrel elevating accordingly

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -1,10 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankBarrel.h"
-
+#include "Engine/World.h"
 
 void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	// Move the barrel the right amount this frame
 	// Givenn a max elevation speed and the frame time
+
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, +1);
+	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;		// izracunaj pomak cijevi
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;								// dodaj pomak trenutnoj rotaciji cijevi
+	auto Elevation = FMath::Clamp<float>(RawNewElevation, MinElevationDegrees, MaxElevationDegrees);
+
+	SetRelativeRotation(FRotator(Elevation, 0, 0));
 }
